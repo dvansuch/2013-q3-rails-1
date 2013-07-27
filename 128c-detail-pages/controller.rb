8295453a4c3531cbbe97b4eeefc12c779249b2f4
Danielle Van Suchtelen <danielles.travels@gmail.com>
2013-07-27 16:46:00 -0600

    pushing class work

--- 128c-detail-pages/controller.rb
+++ 128c-detail-pages/controller.rb
@@ -1,16 +1,11 @@
 require '../dvc-sinatra.rb'
 
 get "/" do
-  # TODO second: add code here to load @apartments
+  @apartments = Apartment.all
   halt erb(:index)
 end
 
-get "/apartments/1" do
-  # TODO first: add code here to load @apartment
-  halt erb(:show1)
-end
-
-get "/apartments/2" do
-  # TODO first: add code here to load @apartment
-  halt erb(:show2)
+get "/apartments/:x" do
+  @apartment = Apartment.find(params[:x])
+  halt erb(:show)
 end
