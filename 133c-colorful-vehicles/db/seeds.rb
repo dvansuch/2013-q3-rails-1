c6c192d4331fcc25606506184a010f91db4e5a40
Danielle Van Suchtelen <danielles.travels@gmail.com>
2013-07-29 11:05:28 -0600

    committing hw

--- 133c-colorful-vehicles/db/seeds.rb
+++ 133c-colorful-vehicles/db/seeds.rb
@@ -1,7 +1,40 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Color.create(
+	name: "purple",
+	html_color: "6666FF"
+)
+
+Color.create(
+	name: "yellow",
+	html_color: "FFFF66"
+)
+
+Color.create(
+	name: "green",
+	html_color: "66FF66"
+)
+
+Color.create(
+	name: "fuschia",
+	html_color: "CC00FF"
+)
+
+
+Vehicle.create(
+	name: "car",
+	image: "car.png"
+)
+
+Vehicle.create(
+	name: "bike",
+	image: "bike.png"
+)
+
+Vehicle.create(
+	name: "bus",
+	image: "bus.png"
+)
+
+Vehicle.create(
+	name: "plane",
+	image: "plane.png"
+)
