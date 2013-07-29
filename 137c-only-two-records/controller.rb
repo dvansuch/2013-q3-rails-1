4293132e25f8c81f4231377d795158e827765655
Danielle Van Suchtelen <danielles.travels@gmail.com>
2013-07-29 17:01:03 -0600

    committing hw

--- 137c-only-two-records/controller.rb
+++ 137c-only-two-records/controller.rb
@@ -4,3 +4,16 @@ get "/" do
   @bracelet_lines = BraceletLine.order("id").all
   halt erb(:root)
 end
+
+get "/line1" do
+  @bracelet_line = BraceletLine.find(1)
+  halt erb(:edit)
+end
+
+get "/line2" do
+  @bracelet_line = BraceletLine.find(2)
+  halt erb(:edit)
+end
+
+#Add GET routes for /line1 and /line2 
+#that render ERb to look similar to mockups/2.png.
