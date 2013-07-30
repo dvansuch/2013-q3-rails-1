2f2d6915640504012188381a669350200e1802e5
Danielle Van Suchtelen <danielles.travels@gmail.com>
2013-07-29 18:31:36 -0600

    committing hw

--- 138c-goat-crud-no-delete/controller.rb
+++ 138c-goat-crud-no-delete/controller.rb
@@ -4,3 +4,13 @@ get "/" do
 	@goats = RacingGoat.all
 	halt erb(:index)
 end
+
+get "/new-goat" do
+	@goat = RacingGoat.new
+	halt erb(:edit)
+end
+
+get "/edit/:id" do
+	@goat = RacingGoat.find(params[:id])
+	halt erb(:edit)
+end
