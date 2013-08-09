a3d5a7e57a130cdbab5583330870d0e0a9e4318b
Danielle Van Suchtelen <danielles.travels@gmail.com>
2013-08-09 10:43:46 -0600

    committing hw

--- 148c-b-cycle-stations/controller.rb
+++ 148c-b-cycle-stations/controller.rb
@@ -4,3 +4,23 @@ get "/" do
   @stations = Station.order(:station_name).page(params[:page]).all
   halt erb(:index)
 end
+
+get "/planned" do
+  @stations = Station.order(:station_name).where(status: "planned").page(params[:page]).all
+  halt erb(:index)
+end
+
+get "/existing" do
+  @stations = Station.order(:station_name).where(status: "existing").page(params[:page]).all
+  halt erb(:index)
+end
+
+get "/zip/80205" do
+  @stations = Station.order(:station_name).where(zip_code: "80205").page(params[:page]).all
+  halt erb(:index)
+end
+
+get "/zip/80209" do
+  @stations = Station.order(:station_name).where(zip_code: params[:zip_code]).page(params[:page]).all
+  halt erb(:index)
+end
