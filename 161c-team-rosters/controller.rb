d8faf43c8be48904c95b1af1111665ad9ad7c21e
Danielle Van Suchtelen <danielles.travels@gmail.com>
2013-08-12 16:50:56 -0600

    post for index page and updated erbs

--- 161c-team-rosters/controller.rb
+++ 161c-team-rosters/controller.rb
@@ -5,8 +5,25 @@ get "/" do
 	halt erb(:index)
 end
 
+post "/" do
+	@team = Team.all
+	@color = Team.where(color: params[:color])
+
+	@new_team = params[:new_team_color]
+	if @new_team != ""
+		color = Team.new
+		color.color = params[:new_team_color]
+		color.save
+		redirect "/"
+	else
+		@error = "Please enter a new color"
+		halt erb(:index)
+	end
+end
+
 get "/team/:color" do
 	@color = Team.where(color: params[:color])
 	@member = Member.all
 	halt erb(:team)
 end
+
