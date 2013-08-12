bc6e5f348f3435f6b923f1e1ed0dd3728a7d51b5
Danielle Van Suchtelen <danielles.travels@gmail.com>
2013-08-12 16:24:16 -0600

    main page links work, get params function

--- 161c-team-rosters/controller.rb
+++ 161c-team-rosters/controller.rb
@@ -1,9 +1,12 @@
 require '../dvc-sinatra.rb'
 
 get "/" do
+	@team = Team.all 
 	halt erb(:index)
 end
 
 get "/team/:color" do
-	
+	@color = Team.where(color: params[:color])
+	@member = Member.all
+	halt erb(:team)
 end
