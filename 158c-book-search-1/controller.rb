19456c960918d62aac44560cb5c277bdc7a19ebd
Danielle Van Suchtelen <danielles.travels@gmail.com>
2013-08-12 09:38:32 -0600

    committing hw

--- 158c-book-search-1/controller.rb
+++ 158c-book-search-1/controller.rb
@@ -5,11 +5,23 @@ get "/" do
 end
 
 get "/year/:year" do
-  # TODO: Finish year route by
-  #       - assigning to @books
-  #       - assigning to @header
+	@books = Book.where(publication_year: params[:year]).all
+	@header = "Books published in #{params[:year]}"
   halt erb(:index)
 end
+
+get "/topic/:topic" do
+	@books = Book.where("title ilike ?", "%#{params[:topic]}%").all
+	@header = "Books with keyword #{params[:topic]}"
+	halt erb(:index)
+end
+
+get "/author/:author" do
+	@books = Book.where(author: params[:author])
+	@header = "Books published by #{params[:author]}"
+  halt erb(:index)
+end
+
 
 # TODO: Write author route
 
