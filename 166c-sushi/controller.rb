7fdc2229076d46220cbf4a1ac59457e70dd350fb
Danielle Van Suchtelen <danielles.travels@gmail.com>
2013-08-19 09:41:32 -0600

    committing hw

--- 166c-sushi/controller.rb
+++ 166c-sushi/controller.rb
@@ -10,7 +10,27 @@ get "/place_order" do
 end
 
 post "/place_order" do
-  # TODO: Write this
+  if params[:commit] == "Place order"
+  	order = Order.new 
+    order.table_number = params[:table_number]
+    order.total_price = 0.0
+    order.save
+
+    Dish.all.each do |dish|
+      if params["quantity_for_dish_#{dish.id}"] != ""
+        line_item = LineItem.new
+        line_item.dish_id = dish.id
+        line_item.order_id = order.id
+        line_item.line_price = dish.price * quantity.to_i
+        line_item.quantity = params["quantity_for_dish_#{dish.id}"]
+        line_item.save!
+
+        order.total_price = order.total_price + line_item.line_price
+      end
+
+    order.save!
+    end
+  end
 
   redirect "/"
 end
