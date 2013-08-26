9100a4c5b2801189232bae37450f4a59c8991222
Danielle Van Suchtelen <danielles.travels@gmail.com>
2013-08-26 14:13:11 -0600

    committing inclass hw

--- 181c-restaurant-rails/db/seeds.rb
+++ 181c-restaurant-rails/db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Entree.create! id: 1, name: "Chicken Marsala sauteed with Mushrooms and Wine", price: 21.95
+Entree.create! id: 2, name: "Grilled Filet Mignon with Port Wine Reduction", price: 29.95
+Entree.create! id: 3, name: "Seared Salmon Filet with Oven Roasted Tomato and Leek Cream Sauce", price: 24.95
+Entree.create! id: 4, name: "Lobster Ravioli", price: 26.95
+Entree.create! id: 5, name: "Pasta Primavara", price: 18.00
+
+Table.create! id: 1, number_of_chairs: 2
+Table.create! id: 2, number_of_chairs: 2
+Table.create! id: 3, number_of_chairs: 2
+Table.create! id: 4, number_of_chairs: 4
+Table.create! id: 5, number_of_chairs: 6
+Table.create! id: 6, number_of_chairs: 6 
+Table.create! id: 7, number_of_chairs: 4
